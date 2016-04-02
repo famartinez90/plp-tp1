4705c1fec7dfee84507fc2635394795aae241785
famartinez90 <famfede@gmail.com>
2016-04-02 16:06:56 -0300

    Initial commit

---
+++ Tp.hs
@@ -0,0 +1,65 @@
+module Tp where
+
+import Data.List
+
+type Texto = String
+type Feature = Float
+type Instancia = [Feature]
+type Extractor = (Texto -> Feature)
+
+type Datos = [Instancia]
+type Etiqueta = String
+type Modelo = (Instancia -> Etiqueta)
+type Medida = (Instancia -> Instancia -> Float)
+
+tryClassifier :: [Texto] -> [Etiqueta] -> Float
+tryClassifier x y = let xs = extraerFeatures ([longitudPromedioPalabras, repeticionesPromedio] ++ frecuenciaTokens) x in
+    nFoldCrossValidation 5 xs y
+
+mean :: [Float] -> Float
+mean xs = realToFrac (sum xs) / genericLength xs
+
+split :: Eq a => a -> [a] -> [[a]]
+split = flip tilps
+
+tilps :: Eq a => [a] -> a -> [[a]]
+tilps = foldr (\x r -> \n -> if x==n then []:(r n) else ((x:head(r n)):(tail(r n)))) (const[[]])
+	
+longitudPromedioPalabras :: Extractor
+longitudPromedioPalabras = (\t ->  mean (map genericLength (split ' ' t)) )
+
+cuentas :: Eq a => [a] -> [(Int, a)]
+cuentas = undefined
+
+repeticionesPromedio :: Extractor
+repeticionesPromedio = undefined --(\t -> )
+
+tokens :: [Char]
+tokens = "_,)(*;-=>/.{}\"&:+#[]<|%!\'@?~^$` abcdefghijklmnopqrstuvwxyz0123456789"
+
+frecuenciaTokens :: [Extractor]
+frecuenciaTokens = undefined
+
+normalizarExtractor :: [Texto] -> Extractor -> Extractor
+normalizarExtractor = undefined
+
+extraerFeatures :: [Extractor] -> [Texto] -> Datos
+extraerFeatures = undefined
+
+distEuclideana :: Medida
+distEuclideana = undefined
+
+distCoseno :: Medida
+distCoseno = undefined
+
+knn :: Int -> Datos -> [Etiqueta] -> Medida -> Modelo
+knn = undefined
+
+accuracy :: [Etiqueta] -> [Etiqueta] -> Float
+accuracy = undefined
+
+separarDatos :: Datos -> [Etiqueta] -> Int -> Int -> (Datos, Datos, [Etiqueta], [Etiqueta])
+separarDatos = undefined
+
+nFoldCrossValidation :: Int -> Datos -> [Etiqueta] -> Float
+nFoldCrossValidation = undefined
