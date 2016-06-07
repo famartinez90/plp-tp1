ebde094c50cb74919f25b041b0fe5c3650bf038c
famartinez90 <famfede@gmail.com>
2016-06-07 10:24:38 -0300

    Agrega el resto de las restricciones

--- plp-tp2/esqueleto-diccionarios/tp.pl
+++ plp-tp2/esqueleto-diccionarios/tp.pl
@@ -50,12 +50,17 @@ palabras([],[[]]).
 palabras([L|Ls],[R|C]):- L \== espacio, palabras(Ls,Rs), head(Rs,T),tail(Rs,C), append([L],T,R).
 palabras([L|Ls],[[]|Rs]):- L == espacio, palabras(Ls,Rs).
 
+% head(+X, ?Y)
 head([X|_],X).
+% head(+X, ?Y)
 tail([_|Xs],Xs).
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % Ejercicio 4
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% asignar_var(+X, ?Y, ?Z)
+% Mapeo inicial o final pueden venir o no instanciados, pero no ambos sin
+% instanciar a la vez.
 asignar_var(A,Mi,Mi):- member((A,_),Mi).
 asignar_var(A,Mi,[(A,_)|Mi]):- not(member((A,_),Mi)).
 
@@ -63,6 +68,7 @@ asignar_var(A,Mi,[(A,_)|Mi]):- not(member((A,_),Mi)).
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % Ejercicio 5
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% palabras_con_variables(+X, -Y)
 palabras_con_variables([],[]).
 palabras_con_variables(Xs,Rs):-asignar_var_list_list(Xs,[],Mp), palabras_con_variables_aux(Xs,Rs,Mp). 
 
@@ -85,7 +91,7 @@ asignar_var_list([X|Xs],Mi,Mt):- asignar_var_list(Xs,Mi,Mf), asignar_var(X,Mf,Mt
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % Ejercicio 6
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
+% quitar(+X, +Y, -Z)
 quitar(_,[],[]).
 quitar(E,[L|Ls],R):- E==L, quitar(E,Ls,R).
 quitar(E,[L|Ls],[L|R]):- E\==L, quitar(E,Ls,R).
@@ -93,18 +99,17 @@ quitar(E,[L|Ls],[L|R]):- E\==L, quitar(E,Ls,R).
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % Ejercicio 7
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% cant_distintos(+X, -Y)
 cant_distintos([],0).
 cant_distintos([L|Ls],N):- quitar(L,Ls,R), cant_distintos(R,M), N is M+1.
 
-% las devoluciones de panes se graban pero depende del formato. Si
-% es franquicia o consecion no, no las graban al menos que autoricen la devolcion si? las de red
-% si, son las que graban.
-
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %Ejercicio 8
-
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% ESTO NO ESTA ANDANDO
 descifrar(S, M):- palabras(S, P), palabras_con_variables(P, V), listas_de_diccionario(V), juntar_con(V, 32, M).
 
-% ESTO NO ESTA ANDANDO
+
 listas_de_diccionario([L]):- diccionario_lista(L).
 listas_de_diccionario([L|Ls]):- diccionario_lista(L), listas_de_diccionario(Ls).
 
